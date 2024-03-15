0c3b4a9e16ad1106262dd6efbc73e7dffeeefd15
Damian-Polakovs <s00236491@atu.ie>
2024-03-15 23:56:04 +0000

    Week 8 nearly complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IOMDBResponse } from './omdbresponse';
+import { OmdbApiService } from './services/omdb-api.service';
+import { SearchtitleComponent } from './components/searchtitle/searchtitle.component';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OmdbApiService,SearchtitleComponent,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/services/omdb-api.service.ts
+++ src/app/services/omdb-api.service.ts
@@ -4,6 +4,7 @@ import{HttpClient,HttpErrorResponse} from "@angular/common/http";
 import {Observable,throwError} from 'rxjs';
 import {catchError,tap} from 'rxjs';
 import {IOMDBResponse} from '../omdbresponse';
+import { IOMDBResponse2 } from '../omdbmresponse2';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +12,15 @@ import {IOMDBResponse} from '../omdbresponse';
 export class OmdbApiService {
   private _siteURL="https://www.omdbapi.com/"
   private _key="bd621f25"
-  private handleError(err:HttpErrorResponse){
-    console.log('OmdbApiService:' + err.message);
-    return throwError(()=>new Error("OmdbApiService:" + err.message))
-  }
+  private _key2="?apikey=fa53e047&s="
+ 
 
   constructor(private _http:HttpClient) { }
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    console.error('OmdbApiService:', err);
+    return throwError(`OmdbApiService: ${err.error}`);
+  }
 
   getMovieData(movieName:string):Observable<IOMDBResponse>{
     return this._http.get<IOMDBResponse>(this._siteURL+ this._key + movieName)
